77ff9a8e884afc1dbce56f3b2575f1dae2b9cc09
AnatolyiTo <tungaznation@gmail.com>
2022-07-09 00:24:26 +0300

    homework8

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector(".mobile-menu__open-btn"),
+    closeMenuBtn: document.querySelector(".mobile-menu__close-btn"),
+    menu: document.querySelector(".mobile-menu"),
+    body: document.querySelector("body")
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleModal() {
+    refs.menu.classList.toggle("is-hidden");
+  }
+})();
